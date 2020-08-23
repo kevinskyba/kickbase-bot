6afd045c83bcf41c65d463846da3af4d6bb51124
kevinskyba <kevinskyba@live.de>
2020-08-23 15:25:09 +0200

    updated version to 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Kickbase_Bot',
-    version='0.0.10',
+    version='0.0.11',
     packages=['kickbase_bot'],
     url='https://github.com/kevinskyba/kickbase-bot',
     license='MIT',
