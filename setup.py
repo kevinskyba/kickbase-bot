f8599e2eb4698edfc156f41efcf71093790b387f
kevinskyba <kevinskyba@live.de>
2020-08-25 19:40:37 +0200

    update version to 0.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Kickbase_Bot',
-    version='0.0.13',
+    version='0.0.14',
     packages=['kickbase_bot'],
     url='https://github.com/kevinskyba/kickbase-bot',
     license='MIT',
